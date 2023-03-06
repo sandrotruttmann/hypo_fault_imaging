d37b5db4a5ec7a0524101721d462c2be65e3b20e
Sandro Truttmann <sandro.truttmann@gmail.com>
2023-03-06 18:54:22 +0800

    set kappa value for extremely well-defined fault planes at largest obtained value

--- Code/fault_network.py
+++ Code/fault_network.py
@@ -743,17 +743,19 @@ def faultnetwork3D(hypo_file, hypo_sep, out_dir, n_mc, r_nn, dt_nn, validation_b
                 v_sum = np.array([np.nansum(nor_x), np.nansum(nor_y), np.nansum(nor_z)])
                 R = np.linalg.norm(v_sum)
                 RN = R / N
-                
+
                 # Calculate FB5 (Kent) distribution parameters
                 vectors = np.array([nor_x, nor_y, nor_z]).T
                 vectors = vectors[~np.isnan(vectors).any(axis=1)]
                 try:
                     G = kent_me(vectors)
+                    mean_vector = G.gamma1 / np.linalg.norm(G.gamma1)
+                    kappa = int(G.kappa)
+                    beta = int(G.beta)
                 except AssertionError:
-                    pass
-                mean_vector = np.array([np.nanmean(nor_x), np.nanmean(nor_y), np.nanmean(nor_z)])  # TODO: get mean vector
-                kappa = 9999999
-                beta = 9999999
+                    mean_vector = np.array([np.nanmean(nor_x), np.nanmean(nor_y), np.nanmean(nor_z)])
+                    kappa = -999
+                    beta = -999
                 
                 # Check if mean lies on upper hemisphere and turn back to lower
                 if mean_vector[2] > 0:
@@ -795,6 +797,10 @@ def faultnetwork3D(hypo_file, hypo_sep, out_dir, n_mc, r_nn, dt_nn, validation_b
                                         'nor_z_mean', 'nr_fits', 'R', 'N', 'R/N',
                                         'kappa', 'beta'])
     data_output = pd.concat([data_output, df_dirstats], axis=1)
+    
+    # Set back the negative (=very well-defined) kappa value to the largest value obtained
+    max_kappa = np.nanmax(data_output['kappa'])
+    data_output['kappa'] = data_output['kappa'].replace(-999, max_kappa)
 
     # Convert the normal unit vector to azimuth and dip of the mean plane
     nor_x_mean = data_output['nor_x_mean']
